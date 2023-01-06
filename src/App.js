14533e1bd724c43bb7999a1e83f1513906e2a260
Shashwat Pandey <cool17shashwat@gmail.com>
2023-01-06 17:13:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,22 +8,22 @@ const App = () => {
 	const [notes, setNotes] = useState([
 		{
 			id: nanoid(),
-			text: 'This is my first post!',
+			text: 'This is my first note!',
 			date: '15/04/2021',
 		},
 		{
 			id: nanoid(),
-			text: 'This is my second post!',
+			text: 'This is my second note!',
 			date: '21/04/2021',
 		},
 		{
 			id: nanoid(),
-			text: 'This is my third post!',
+			text: 'This is my third note!',
 			date: '28/04/2021',
 		},
 		{
 			id: nanoid(),
-			text: 'This is my new post!',
+			text: 'This is my new note!',
 			date: '30/04/2021',
 		},
 	]);
